5e48a30e511c5537efdcf3b7121546ffb9b6d7c6
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-09-30 13:08:06 +0000

    Scraping main page requires user agent

--- Scraper.cs
+++ Scraper.cs
@@ -1,5 +1,17 @@
 public record Scraper(string BaseURL, string Password) {
     public async Task Run() {
-        await Console.Out.WriteLineAsync($"Scraping images from {BaseURL}");
+        Console.WriteLine($"Scraping images from {BaseURL}");
+
+        var client = new HttpClient() {
+            BaseAddress = new Uri(BaseURL)
+        };
+        client.DefaultRequestHeaders.UserAgent.Clear();
+        client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("famini-scraper", "1.0"));
+
+        var mainPageContent = await client.GetAsync("/");
+        
+        string content = await mainPageContent.Content.ReadAsStringAsync();
+
+        Console.WriteLine(content);
     }
 }
