066bb5a20a382aef32fc1720d6ac4374d3fd1a2b
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-09-30 12:32:31 +0000

    Showing usage, adding extensions

--- FaminiScraper/Program.cs
+++ FaminiScraper/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length == 0) {
+    Console.WriteLine("Usage: FaminiScraper outputDirectory");
+}
+
