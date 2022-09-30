4ae6c6dbc513ad7e8b2a66099b58ca3a5d36ef00
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-09-30 15:51:55 +0000

    Creating blob service client

--- FaminiUploader.cs
+++ FaminiUploader.cs
@@ -1,5 +1,13 @@
+
+
+using Azure.Storage;
+using Azure.Storage.Blobs;
+
 public record FaminiUploader(string BaseDirectory, string AccountName, string AccountKey) {
     public async Task UploadImages() {
+        var sharedKeyCredential = new StorageSharedKeyCredential(AccountName, AccountKey);
+        string blobUri = "https://" + AccountName + ".blob.core.windows.net";
+        var blobServiceClient = new BlobServiceClient(new Uri(blobUri), sharedKeyCredential);
         var files = Directory.EnumerateFiles(BaseDirectory);
 
         foreach (var fileName in files) {
