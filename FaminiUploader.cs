67bb790518f41565faff56dc233a739b928849e3
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2022-09-30 18:56:25 +0000

    Finishing uploader

--- FaminiUploader.cs
+++ FaminiUploader.cs
@@ -8,10 +8,20 @@ public record FaminiUploader(string BaseDirectory, string AccountName, string Ac
         var sharedKeyCredential = new StorageSharedKeyCredential(AccountName, AccountKey);
         string blobUri = "https://" + AccountName + ".blob.core.windows.net";
         var blobServiceClient = new BlobServiceClient(new Uri(blobUri), sharedKeyCredential);
+
+        var blobContainers = blobServiceClient.GetBlobContainers();
+        var photosContainer = blobContainers.FirstOrDefault(bc => bc.Name == "photos");
+        if (photosContainer == null) {
+            var createContainerResponse = await blobServiceClient.CreateBlobContainerAsync("photos");
+        }
+
+        var containerClient = blobServiceClient.GetBlobContainerClient("photos");
+
         var files = Directory.EnumerateFiles(BaseDirectory);
 
         foreach (var fileName in files) {
-            await Console.Out.WriteLineAsync(fileName);
+            var blobClient = containerClient.GetBlobClient(Path.GetFileName(fileName));
+            await blobClient.UploadAsync(fileName, true);
         }
     }
 }
